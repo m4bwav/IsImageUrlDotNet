fac2f3bf90e7cb8316bf8ad658fe9f3fdd463104
m4bwav <m4bwav@gmail.com>
2016-05-29 14:41:04 -0500

    Initial check-in.

---
+++ IsImageUrlDotNet.Test/IsImageUrlDotNetLibBehavior.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IsImageUrlDotNet.Test
+{
+    [TestClass]
+    public class IsImageUrlDotNetLibBehavior
+    {
+        private const string CommonImageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+        private const string NotAnImageUrl = "https://www.google.com";
+        private const string ImageUrlExmapleWithoutExtension = "http://placehold.it/350x150";
+        private const string PdfUrlExample = "http://www.sanface.com/pdf/test.pdf";
+
+        [TestMethod]
+        public void ShouldBeAbleToIdentifyACommonImageUrl()
+        {
+            var isImage = CommonImageUrl.IsImageUrl();
+
+            Assert.IsTrue(isImage);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToIdentifyAnExtensionlessImageUrl()
+        {
+            var isImage = ImageUrlExmapleWithoutExtension.IsImageUrl();
+
+            Assert.IsTrue(isImage);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToTellIfACommonUrlIsNotForAnImageUrl()
+        {
+            var isImage = NotAnImageUrl.IsImageUrl();
+
+            Assert.IsFalse(isImage);
+        }
+        [TestMethod]
+        public void ShouldBeAbleToTellThatACommonPdfUrlIsNotAnImageUrl()
+        {
+            var isImage = PdfUrlExample.IsImageUrl();
+
+            Assert.IsFalse(isImage);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToAccessAsAStaticMethod()
+        {
+            // ReSharper disable once InvokeAsExtensionMethod
+            var isImage = IsImageUrlDotNetLib.IsImageUrl(CommonImageUrl);
+
+            Assert.IsTrue(isImage);
+        }
+    }
+}
---
+++ IsImageUrlDotNet/MainCode.fs
@@ -0,0 +1,43 @@
+﻿namespace IsImageUrlDotNet
+open System.Net
+open System
+open System.IO
+
+
+[<System.Runtime.CompilerServices.Extension>]
+module IsImageUrlDotNetLib =   
+    let ImageFileExtensions = ["png"; "jpg"; "gif"; "raw";"bmp"; "svg"; "gif"; "raw";]
+    let NonImageFileExtensions = ["exe"; "pdf"; "html"; "htm"; "txt"; "mp3"; "wav"; "odp";]
+    let private hasAnImageFileExtension(url:string) =
+      if Path.HasExtension url = false then
+         false
+      else  
+         let urlSplitList = url.ToLower().Split '.'
+         let lastExtension = urlSplitList.[urlSplitList.Length - 1]
+         List.contains lastExtension ImageFileExtensions
+
+    let private hasAnNonImageFileExtension(url:string) =
+      if Path.HasExtension url = false then
+         false
+      else  
+         let urlSplitList = url.ToLower().Split '.'
+         let lastExtension = urlSplitList.[urlSplitList.Length - 1]
+         List.contains lastExtension NonImageFileExtensions
+
+    let private requestUrlAndCheckIfImage(url:string) =
+        let req = WebRequest.Create(Uri(url)) 
+        use resp = req.GetResponse() 
+        let contentType = resp.ContentType
+        if contentType.Contains("text/html") then
+           false
+        else
+            contentType.Contains("image")
+
+    [<System.Runtime.CompilerServices.Extension>]   
+    let IsImageUrl(opt:string) =                
+      match opt with
+        | null | "" -> false
+        | url when Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute) = false -> false
+        | url when hasAnImageFileExtension url -> true 
+        | url when hasAnNonImageFileExtension url -> false 
+        | _ -> requestUrlAndCheckIfImage opt         
