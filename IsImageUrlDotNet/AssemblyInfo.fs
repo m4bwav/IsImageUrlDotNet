139f55730e82d70a923c5d8de06d700e0df5c2f8
m4bwav <m4bwav@gmail.com>
2016-05-29 20:40:44 -0500

    Nuget package spec

--- IsImageUrlDotNet/AssemblyInfo.fs
+++ IsImageUrlDotNet/AssemblyInfo.fs
@@ -8,9 +8,9 @@ open System.Runtime.InteropServices
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [<assembly: AssemblyTitle("IsImageUrlDotNet")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyDescription("Extension and Static method to check if a url is for an image.")>]
 [<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyCompany("Mark Rogers")>]
 [<assembly: AssemblyProduct("IsImageUrlDotNet")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("1.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0")>]
 
 do
     ()
